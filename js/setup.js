b1238a9f264b637688827bb792ea86a706a21d62
hbilkiss <hbilkiss@sas.upenn.edu>
2026-04-27 11:47:53 -0400

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,85 +1,75 @@
+// Create the map
 var map = L.map('map', {
   center: [48.85383223396097, 2.328864447526347],
-  zoom: 18,
+  zoom: 18
 });
 
-// Search Addresses
+// Search address control
 L.Control.geocoder({
   defaultMarkGeocode: false
 })
-  .on('markgeocode', function(e) {
-    map.fitBounds(e.geocode.bbox);
-  })
-  .addTo(map);
-
-// OPTIONAL: this moves the map to your current physical location.
-// Comment it out if you want the map to stay centered on Sciences Po.
-// map.locate({ setView: true, maxZoom: 18 });
-
-// map.on('locationfound', function(e) {
-//   L.marker(e.latlng).addTo(map)
-//     .bindPopup("You are here").openPopup();
-// });
+.on('markgeocode', function(e) {
+  map.fitBounds(e.geocode.bbox);
+})
+.addTo(map);
 
 // Scale bar
 L.control.scale().addTo(map);
 
-// --- Basemap options ---
-var EsriSat = L.tileLayer(
+// Basemap layers
+var EsriSatellite = L.tileLayer(
   'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-  { attribution: 'Tiles © Esri' }
+  {
+    attribution: 'Tiles © Esri'
+  }
 );
 
 var CartoLight = L.tileLayer(
   'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
-  { attribution: '&copy; OpenStreetMap contributors & CARTO' }
+  {
+    attribution: '&copy; OpenStreetMap contributors & CARTO'
+  }
 );
 
 var CartoDark = L.tileLayer(
   'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
-  { attribution: '&copy; OpenStreetMap contributors & CARTO' }
+  {
+    attribution: '&copy; OpenStreetMap contributors & CARTO'
+  }
 );
 
-var StamenTerrain = L.tileLayer(
-  'https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
-  { attribution: 'Map tiles by Stamen Design — Data © OpenStreetMap' }
+var OpenStreetMap = L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; OpenStreetMap contributors'
+  }
 );
 
-var EsriTopo = L.tileLayer(
-  'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
-  { attribution: 'Tiles © Esri' }
-);
+// Add default basemap
+EsriSatellite.addTo(map);
 
-// Default basemap
-EsriSat.addTo(map);
-
-var baseMaps = {
-  "Satellite Esri": EsriSat,
-  "CARTO Light": CartoLight,
-  "CARTO Dark": CartoDark,
-  "Stamen Terrain": StamenTerrain,
-  "Esri Topo": EsriTopo
-};
-
-L.control.layers(baseMaps).addTo(map);
-
-// Style for GeoJSON building polygon
+// Style for the Sciences Po polygon
 var myStyle = {
   fillColor: "#ff7800",
-  color: "#000",
-  weight: 2,
+  color: "yellow",
+  weight: 4,
   opacity: 1,
-  fillOpacity: 0.7
+  fillOpacity: 0.75
 };
 
-// Load Sciences Po polygon from tyler.js
-L.geoJSON(tyler, {
+// Add the GeoJSON polygon from tyler.js
+var sciencesPoPolygon = L.geoJSON(tyler, {
   style: myStyle
 })
-  .bindPopup(function(layer) {
-    return layer.feature.properties.name;
-  })
-  .addTo(map);
+.bindPopup(function(layer) {
+  return layer.feature.properties.name;
+})
+.addTo(map);
+
+// Zoom directly to the polygon so you can see it
+map.fitBounds(sciencesPoPolygon.getBounds(), {
+  padding: [30, 30]
+});
 
 // Circle near Sciences Po
 var circle = L.circle([48.85405, 2.32865], {
@@ -87,16 +77,36 @@ var circle = L.circle([48.85405, 2.32865], {
   fillColor: '#f03',
   fillOpacity: 0.5,
   radius: 15
-}).addTo(map).bindPopup("Study area circle");
+})
+.addTo(map)
+.bindPopup("Study area circle");
 
 // Marker: Sciences Po
-L.marker([48.85417442591539, 2.3283007287517545])
-  .addTo(map)
-  .bindPopup("Sciences Po College Universitaire");
+var sciencesPoMarker = L.marker([48.85417442591539, 2.3283007287517545])
+.addTo(map)
+.bindPopup("Sciences Po College Universitaire");
 
 // Marker: Café Noir
-L.marker([48.85505929226488, 2.32632017043754])
-  .addTo(map)
-  .bindPopup("My favorite cafe: Café Noir");
+var cafeMarker = L.marker([48.85505929226488, 2.32632017043754])
+.addTo(map)
+.bindPopup("My favorite cafe: Café Noir");
+
+// Layer control
+var baseMaps = {
+  "Satellite": EsriSatellite,
+  "CARTO Light": CartoLight,
+  "CARTO Dark": CartoDark,
+  "OpenStreetMap": OpenStreetMap
+};
+
+var overlayMaps = {
+  "Sciences Po Polygon": sciencesPoPolygon,
+  "Study Area Circle": circle,
+  "Sciences Po Marker": sciencesPoMarker,
+  "Café Noir Marker": cafeMarker
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 console.log("Map loaded successfully!");
+console.log("GeoJSON layer:", tyler);
